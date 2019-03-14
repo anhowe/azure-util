436835db3c8ebc99b2c2743f4c71f321eabb31f8
Anthony Howe <ahowe_ca@yahoo.ca>
2019-03-13 20:40:37 -0400

    add linux deployment with 10 minute cse

---
+++ linux-with-10-min-cse/sleep10min.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+function sleep_10_minutes() {
+    retries=120
+    apt_update_output=/tmp/apt-get-update.out
+    for i in $(seq 1 $retries); do
+        echo "`date` - tick $i - sleeping for 10 minutes before returning"
+        sleep 5
+    done
+    echo Executed apt-get update $i times
+}
+
+sleep_10_minutes
